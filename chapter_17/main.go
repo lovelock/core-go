e372b145978da98971d2faba42a8cd86fdc3459b
Frost Wong <frostwong@gmail.com>
2018-12-03 21:24:59 +0800

    这样只能保证执行时值已经传入了，但还是没有控制执行顺序

--- chapter_17/main.go
+++ chapter_17/main.go
@@ -9,10 +9,10 @@ func main() {
 
 	sign := make(chan struct{}, num)
 	for i := 0; i < num; i++ {
-		go func() {
+		go func(i int) {
 			fmt.Println(i)
 			sign <- struct{}{}
-		}()
+		}(i)
 	}
 
 	for j := 0; j < num; j++ {
